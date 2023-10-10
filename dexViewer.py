690b945e515578f96ad216252e7d12706bb4cb37
Narmis-E <narmisecurb@gmail.com>
2023-10-10 23:26:50 +1030

    refactored dexViewer

--- dexViewer.py
+++ dexViewer.py
@@ -1,74 +1,85 @@
-import sys
+from pydexcom import Dexcom
+import pandas as pd
 import configparser
+import sys
 import gi
-import datetime as datetime
 import os
-import pandas
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 from gi.repository import Gtk, Gdk, Adw, Gio, GLib
 
-from utils import *
 import dexShareCredentials
 import bgPlotter
-from configHandler import load_credentials
 
-credentials_window = dexShareCredentials.DexShareCredentials(get_glucose, load_credentials)
-plotter = bgPlotter.BgPlotter()
+def load_credentials():
+    config_exists = os.path.isfile('config.ini')
+    if config_exists:
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        if 'Credentials' in config:
+            global username, password, ous
+            username = config['Credentials'].get('Username', '')
+            password = config['Credentials'].get('Password', '')
+            ous = config['Credentials'].get('ous', '')
+            print(f"Loaded credentials for {username}")
+            return username, password, ous
+    return None, None, None
+
+def get_glucose(time_scale):
+    dexcom = Dexcom(username, password, ous) # add ous=True if outside of US
+    df = pd.DataFrame(columns=["BG", "TrendArrow"])
+    bg = dexcom.get_glucose_readings(minutes=time_scale+1)
+    for reading in bg: # loop for the amount of rows of data
+        bg_value = reading.mmol_l
+        trend_arrow = reading.trend_arrow
+        # Create a new DataFrame with the current data point
+        new_data = pd.DataFrame({"BG": [bg_value], "TrendArrow": [trend_arrow]})
+        df = pd.concat([df, new_data], ignore_index=True)
+    df.to_csv("BG_data.csv", index=False)
 
 class MainWindow(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        apply_css()
+        self.apply_css()
         self.set_default_size(800, 400)
         self.set_title("DexViewer")
         self.connect("close-request", self.on_delete_event)
-        self.username, self.password, self.ous = load_credentials()
+        username, password, ous = load_credentials()
+        get_glucose(3*60)
 
-        # Create a header bar
         self.header = Gtk.HeaderBar()
         self.set_titlebar(self.header)
         
         menu = Gio.Menu.new()
-        # Create a popover
-        self.popover = Gtk.PopoverMenu()  # Create a new popover menu
+        self.popover = Gtk.PopoverMenu()
         self.popover.set_menu_model(menu)
 
         # Create a menu button
         self.menu_button = Gtk.MenuButton()
         self.menu_button.set_popover(self.popover)
-        self.menu_button.set_icon_name("open-menu-symbolic")  # Give it a nice icon
+        self.menu_button.set_icon_name("open-menu-symbolic")
         self.menu_button.add_css_class('menu_button')
-
-        # Add menu button to the header bar
         self.header.pack_end(self.menu_button)
-        # Set app name
+        
         GLib.set_application_name("DexViewer")
 
-        # Create an action to run a *show about dialog* function we will create 
         action = Gio.SimpleAction.new("about", None)
         action.connect("activate", self.show_about)
         self.add_action(action)
-        menu.append("About", "win.about")  # Add it to the menu we created in previous section
-        
-        self.add_share_source = Gtk.Button(label="Open")
-        self.add_share_source.add_css_class('add_share_source')
-        self.add_share_source.connect("clicked", self.on_add_share_source_clicked)
-        self.add_share_source.set_icon_name("list-add")
-        self.header.pack_start(self.add_share_source)
+        menu.append("About", "win.about")
         
         self.add_share_source = Gtk.Button(label="Sync")
         self.add_share_source.add_css_class('sync_glucose')
         self.add_share_source.connect("clicked", self.sync_glucose)
         self.add_share_source.set_icon_name("view-refresh-symbolic")
-        self.header.pack_end(self.add_share_source)
+        self.header.pack_start(self.add_share_source)
         
         self.box1 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         self.box2 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.box3 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.set_child(self.box1)
-        self.box1.append(self.box2)  # Put vert box in that box
-        self.box1.append(self.box3)  # And another one, empty for now
+        self.box1.append(self.box2)
+        self.box1.append(self.box3)
         
         self.time_scale_combo = Gtk.ComboBoxText()
         self.time_scale_combo.append("0", "1 Hour")
@@ -78,21 +89,20 @@ class MainWindow(Gtk.ApplicationWindow):
         self.time_scale_combo.set_active(1)
         self.time_scale_combo.connect("changed", self.on_time_scale_changed)
         self.box3.append(self.time_scale_combo)
-
-        self.box2.append(plotter.canvas)
         
-        # Create a Gtk.Label to display the number
+        self.plotter = bgPlotter.BgPlotter()
+        self.box2.append(self.plotter.canvas)
+
         self.number_label = Gtk.Label(label="5.5")
         self.number_label.set_hexpand(True)
         self.number_label.set_vexpand(True)
         self.number_label.set_justify(Gtk.Justification.CENTER)
-        # Add the label to the box3 container
         self.box3.append(self.number_label)
 
     def show_about(self, action, param):
         self.about = Gtk.AboutDialog()
-        self.about.set_transient_for(self)  # Makes the dialog always appear in from of the parent window
-        self.about.set_modal(self)  # Makes the parent window unresponsive while dialog is showing
+        self.about.set_transient_for(self)
+        self.about.set_modal(self)
 
         self.about.set_authors(["Narmis-E\n\nNot possible without the pydexcom API from gagebenne!"])
         self.about.set_copyright("Copyright 2023 Narmis Ecurb")
@@ -100,60 +110,66 @@ class MainWindow(Gtk.ApplicationWindow):
         self.about.set_website("http://github.com/Narmis-E/DexViewer")
         self.about.set_website_label("DexViewer Github")
         self.about.set_version("1.0.0")
-        self.about.set_logo_icon_name("./Dexviewer_circle.png")  # The icon will need to be added to appropriate location
-                                                # E.g. /usr/share/icons/hicolor/scalable/apps/org.example.example.svg
+        self.about.set_logo_icon_name("./Dexviewer_circle.png")
 
         self.about.set_visible(True)
+    
+    def show_credentials(self):
+        if username and password:
+            credentials_window.username_entry.set_text(username)
+            credentials_window.password_entry.set_text(password)
+        if ous == 'True':
+            credentials_window.ous_check.set_active(True)
+        else:
+            credentials_window.ous_check.set_active(False)
 
     def on_delete_event(self, event):
         Gtk.Application.get_default().quit()
 
     def sync_glucose(self, button):
         active_text = self.time_scale_combo.get_active_text()
-        if self.username is not None and self.password is not None:
+        if username is not None and password is not None:
             if active_text == "1 Hour":
-                self.fetch_dexcom_data(1)
-                plotter.set_time_scale(1)
+                get_glucose(1*60)
+                self.plotter.set_time_scale(1)
             elif active_text == "3 Hours":
-                self.fetch_dexcom_data(3)
-                plotter.set_time_scale(3)
+                get_glucose(3*60)
+                self.plotter.set_time_scale(3)
             elif active_text == "6 Hours":
-                self.fetch_dexcom_data(6)
-                plotter.set_time_scale(6)
+                get_glucose(6*60)
+                self.plotter.set_time_scale(6)
             elif active_text == "12 Hours":
-                self.fetch_dexcom_data(12)  
-                plotter.set_time_scale(12)
+                get_glucose(12*60)
+                self.plotter.set_time_scale(12)
         print("Synchronised Blood Glucose")
         
     def on_add_share_source_clicked(self, button):
-        credentials_window = dexShareCredentials.DexShareCredentials(get_glucose, load_credentials)
+        self.show_credentials()
         credentials_window.present()
     
-    def fetch_dexcom_data(self, time_scale):
-        if self.username is not None and self.password is not None:
-            # Call get_glucose with the specified time scale
-            self.time_scale = time_scale
-            get_glucose(self.time_scale*60)
-    
     def on_time_scale_changed(self, combo):
-        # Get the selected time scale
         active_text = combo.get_active_text()
 
         # Update the plot based on the selected time scale
         if active_text == "1 Hour":
-            self.fetch_dexcom_data(1)
-            plotter.set_time_scale(1)
+            get_glucose(1*60)
+            self.plotter.set_time_scale(1)
         elif active_text == "3 Hours":
-            self.fetch_dexcom_data(3)
-            plotter.set_time_scale(3)
+            get_glucose(3*60)
+            self.plotter.set_time_scale(3)
         elif active_text == "6 Hours":
-            self.fetch_dexcom_data(6)
-            plotter.set_time_scale(6)
+            get_glucose(6*60)
+            self.plotter.set_time_scale(6)
         elif active_text == "12 Hours":
-            self.fetch_dexcom_data(12)  
-            plotter.set_time_scale(12)
+            get_glucose(12*60)
+            self.plotter.set_time_scale(12)
+    
+    def apply_css(self):
+        css_provider = Gtk.CssProvider()
+        css_provider.load_from_file(Gio.File.new_for_path('style.css'))
+        Gtk.StyleContext.add_provider_for_display(Gdk.Display.get_default(), css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
-class MyApp(Adw.Application):
+class Viewer(Adw.Application):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.connect('activate', self.on_activate)
@@ -161,7 +177,3 @@ class MyApp(Adw.Application):
     def on_activate(self, app):
         self.win = MainWindow(application=app)
         self.win.present()
-
-if __name__ == "__main__":
-    app = MyApp(application_id="com.github.Narmis-E.DexViewer")
-    app.run(sys.argv)
