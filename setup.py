a6fa24ad58bcdf01c13ff70c7ef9061d521b68c1
Narmis-E <narmisecurb@gmail.com>
2023-10-12 12:46:13 +1030

    added setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dexviewer",
+    version="1.0.0",
+    description="A GTK-4 interface for viewing Dexcom CGM data from the pydexcom APIs",
+    author="Narmis-E",
+    author_email="narmisecurb@gmail.com",
+    url="https://github.com/narmis-e/DexViewer",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+      "PyGObject",
+      "pandas",
+      "pydexcom",
+      "matplotlib",
+    ],
+    entry_points={
+      'console_scripts': [
+        'dexviewer = dexviewer.__main__:main',
+      ],
+    },
+    classifiers=[
+      "Intended Audience :: End Users/Desktop",
+      "License :: GNU General Public License v3 (GPLv3)",
+      "Operating System :: POSIX :: Linux",
+      "Programming Language :: Python :: 3",
+      "Topic :: Desktop Environment",
+      "Topic :: Utilities",
+    ],
+    license="GPLv3",
+)
