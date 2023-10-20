3a49359a534552a2c56de07f56c1571670652e8c
Narmis-E <narmisecurb@gmail.com>
2023-10-20 11:08:14 +1030

    added output for paths

--- setup_dexviewer.sh
+++ setup_dexviewer.sh
@@ -13,8 +13,10 @@ desktop_path=$(find "/home/`sh -c 'echo $SUDO_USER'`/.local/lib" -name "dexviewe
 if [ -z "$svg_path" ] || [ -z "$desktop_path" ]; then
     echo "Could not locate dexviewer.svg or dexviewer.desktop files in the user's home directory."
     exit 1
+else
+    echo "found dexviewer.svg at $svg_path"
+    echo "found dexviewer.desktop at $desktop_path" 
+    cp "$svg_path" /usr/share/icons/hicolor/scalable/apps/
+    cp "$desktop_path" /usr/share/applications
+    echo "Copied logo and .desktop file. You can now use your GUI to run dexviewer."
 fi
-
-cp "$svg_path" /usr/share/icons/hicolor/scalable/apps/
-cp "$desktop_path" /usr/share/applications
-echo "Copied logo and .desktop file. You can now use your GUI to run dexviewer."
