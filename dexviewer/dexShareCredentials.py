0811f191ef6504d01da196e4c7f57129a166273b
Narmis-E <narmisecurb@gmail.com>
2023-10-21 16:54:52 +1030

    added error handling for dexcom share credential entry

--- dexviewer/dexShareCredentials.py
+++ dexviewer/dexShareCredentials.py
@@ -1,6 +1,8 @@
 import sys
 import configparser
 import gi
+from pydexcom import Dexcom
+from pydexcom import errors
 import os
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
@@ -10,9 +12,13 @@ class DexShareCredentials(Gtk.ApplicationWindow):
     credentials_provided_signal = GObject.Signal(flags=GObject.SignalFlags.RUN_FIRST, return_type=None)
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.set_default_size(300, 200)
+        self.set_default_size(350, 250)
         self.set_resizable(False)
         self.set_modal(True)
+        self.set_title("Dexcom SHARE Credentials:")
+        
+        self.header = Gtk.HeaderBar()
+        self.set_titlebar(self.header)
 
         self.username_entry = Gtk.Entry()
         self.username_entry.set_placeholder_text("Username")
@@ -28,6 +34,16 @@ class DexShareCredentials(Gtk.ApplicationWindow):
         
         self.ous_check = Gtk.CheckButton(label="Outside US")
         self.ous_check.set_active(False)  # By default, set it to False (inside US)
+        
+        pass_toggle = Gtk.CheckButton(label="Show Password")
+        pass_toggle.set_active(False)
+        pass_toggle.connect("toggled", self.pass_toggled)
+        
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        box.set_spacing(10)
+        box.set_halign(Gtk.Align.CENTER)
+        box.set_valign(Gtk.Align.CENTER)
+        self.set_child(box)
 
         grid = Gtk.Grid()
         grid.set_column_spacing(10)
@@ -37,17 +53,50 @@ class DexShareCredentials(Gtk.ApplicationWindow):
         grid.attach(ok_button, 0, 2, 1, 1)
         grid.attach(exit_button, 1, 2, 1, 1)
         grid.attach(self.ous_check, 2, 2, 1, 1)
-        self.set_child(grid)
+        grid.attach(pass_toggle, 2, 3, 2, 1)
+        box.append(grid)
+    
+    def pass_toggled(self, button):
+        is_visible = button.get_active()
+        self.password_entry.set_visibility(is_visible)
 
     def on_ok_clicked(self, button):
         self.username = self.username_entry.get_text()
         self.password = self.password_entry.get_text()
         self.ous = str(self.ous_check.get_active())
+        if not self.username or not self.password:
+            dialog = Gtk.AlertDialog(transient_for=self)
+            dialog.set_message("Invalid Credentials")
+            dialog.set_detail("Credentials cannot be empty.")
+            dialog.set_buttons(["OK"])
+            dialog.show()
+            return  # Return early to prevent further execution
+
+        try:
+            # Attempt to create a Dexcom session
+            dexcom = Dexcom(self.username, self.password, self.ous)
+        except errors.AccountError as e:
+            if "Password not valid" in str(e):
+                # Handle the incorrect password error
+                dialog = Gtk.AlertDialog(transient_for=self)
+                dialog.set_message("Invalid Credentials")
+                dialog.set_detail("The one or more of provided credentials are not valid.")
+                dialog.set_buttons(["OK"])
+                dialog.show()
+            else:
+                # Handle other AccountError cases
+                dialog = Gtk.AlertDialog()
+                dialog.set_message("Invalid Credentials")
+                dialog.set_detail("Please check that your credentials are correct.")
+                dialog.set_buttons(["OK"])
+                dialog.show()
+            return  # Return early if an error occurred
+
         self.save_credentials_to_config()
-        
+
         # Emit the signal to indicate that credentials have been provided
         self.emit("credentials-provided-signal")
-        self.destroy()
+        #self.destroy()
 
     def on_exit_clicked(self, button):
         self.destroy()
